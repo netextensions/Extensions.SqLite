62df80b6044f3f94f5215138ddac0a1e4998b201
Csaba Salat <csalat@globalblue.com>
2020-04-21 15:51:37 +0200

    add sqlite

---
+++ src/Extensions.SqLite/SqLiteExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NetExtensions
+{
+    public static class SqLiteExtension
+    {
+        public static IServiceCollection AddSqlServerDb<TContext>(this IServiceCollection services, string connectionString) where TContext : DbContext, new()
+        {
+            services.AddDbContext<TContext>(c => c.UseSqlite(connectionString));
+            var options = new DbContextOptionsBuilder<TContext>()
+                .UseSqlite(connectionString)
+                .Options;
+
+            using var context = CreateContext(options);
+            context.Database.Migrate();
+
+            services.AddSingleton(sp => options);
+            return services;
+        }
+
+        private static TContext CreateContext<TContext>(DbContextOptions<TContext> options) where TContext : DbContext, new() => (TContext)Activator.CreateInstance(typeof(TContext), options);
+
+    }
+}
